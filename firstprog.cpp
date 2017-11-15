fa935f56f669651834b935a8c071b5a851a5ec11
JdaMenace <jdeminnis@gmail.com>
2017-11-15 11:04:51 -0500

    Add Lab1
    Add everything from Lab1 from COSC120

---
+++ firstprog.cpp
@@ -0,0 +1,20 @@
+// This is the first program that just writes out a simple message
+// Justin DeMinnis
+
+#include <iostream>               // needed to perform C++ I/O
+using namespace std;
+
+int main ()
+
+{
+
+	cout << "Now is the time for all good men" << endl;
+	cout << "To come to the aid of their party" << endl;
+
+	return 0;
+
+}
+
+//Now is the time for all good men
+//To come to the aid of their party
+//Press any key to continue . . .
